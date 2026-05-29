b35cb5d0fa8a79e256533d1f5a3416a0cecb383b
saadifarooq <saadifarooq@hotmail.com>
2026-05-29 03:31:57 -0400

    Refactor app.py to simplify imports and update index route

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for, session, flash
-import pandas as pd
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY", "change-this-secret")
 
 @app.route("/")
 def index():
-    return "App is running"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    return render_template("index.html")
